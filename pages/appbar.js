248146c229269621230adbd57e84761f0e095d5f
Ishmam <techopoly444@gmail.com>
2023-05-14 02:12:09 +0600

    added to metamask and loaded contract

--- pages/appbar.js
+++ pages/appbar.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 import { AppBar, Toolbar, Button, Typography } from '@mui/material';
 import { makeStyles } from '@mui/styles';
 import { styled } from "@mui/material/styles";
 import { useRouter } from 'next/router';
 import Link from 'next/link';
+import getWeb3 from './Components/getWeb3';
+import mifiContract from '../public/contract/Mifi.json';
+
 const useStyles = makeStyles((theme) => ({
   logo: {
     flexGrow: 1,
@@ -11,11 +14,45 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default function AppHeader() {
+
   const classes = useStyles();
   const router = useRouter();
+  const [web3, setWeb3] = useState(null);
+  const [accounts, setAccounts] = useState([]);
+  const [contract, setContract] = useState(null);
+
+
+  useEffect(() => {
+    const init = async () => {
+      try {
+        const { web3Instance } = await getWeb3();
+        const object = await getWeb3();
+        console.log(object);
+        const accounts = await web3Instance.eth.getAccounts();
+        console.log("acc:", accounts);
+        const networkId = await web3Instance.eth.net.getId();
+        const deployedNetwork = mifiContract.networks[networkId];
+        const instance = new web3Instance.eth.Contract(
+          mifiContract.abi,
+          deployedNetwork && deployedNetwork.address
+        );
+
+        setWeb3(web3Instance);
+        setAccounts(accounts);
+        setContract(instance);
+      } catch (error) {
+        alert("Failed to load web3, accounts, or contract.");
+        console.error(error);
+      }
+    };
+
+    init();
+
+  }, []);
   const handleOptionClick = (option) => {
     router.push(`/${option}`);
   };
+
 
   return (
     <AppBar position="static">
@@ -24,15 +61,18 @@ export default function AppHeader() {
           MicroLoan
         </Typography>
         <nav>
+       
          <Button color="inherit">My Website</Button>
           <Button color="inherit" >About Us</Button>
           <Button color="inherit">Apply for Loan</Button>
           <Button color="inherit">FAQ</Button>
           <Button color="inherit" onClick={() => handleOptionClick('verify')}>Verify</Button>
           <Button color="inherit" onClick={() => handleOptionClick('dashboard')}>Protocol Manager</Button>
-          <Button variant="outlined"color="inherit">Connect Metamask</Button>
-        </nav>
+          <Button variant="outlined" color="inherit">{accounts? "Connected": 'Connect Metamask'}</Button>
+          {accounts && <Button variant="outlined" color="inherit">{accounts}</Button>}
+        </nav> 
       </Toolbar>
+      
     </AppBar>
   );
 }
